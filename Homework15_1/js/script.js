95855b7f56259927bf76634559da6b01e6d1743b
Sergey <sdiomant@gmail.com>
2021-11-15 16:45:34 +0200

    Homework15_1

---
+++ Homework15_1/js/script.js
@@ -0,0 +1,30 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'ru', 'ua'];
+
+select.addEventListener('change', changeURLLanguage);
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    innerHTML = langArr['title1'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+
+    }
+}
+
+changeLanguage();
