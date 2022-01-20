c17535ab1416601683bafd1db298daf08e154008
AnnaSavchenko <anna.pereklad@gmail.com>
2022-01-20 14:26:45 +0200

    task minimum is done

---
+++ Homework30/main.js
@@ -0,0 +1,10 @@
+console.log((0.1 + 0.2).toFixed(1)); 
+
+let a= +"1";
+let b= 2;
+console.log(a+b);
+
+const file = 820;
+let memory = +prompt("Вкажіть об`єм флешки в GB");
+let numberOfFiles = Math.floor(memory *1024 / file);
+alert(numberOfFiles);
