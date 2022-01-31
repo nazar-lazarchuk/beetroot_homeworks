8320f42b26572fe22157f068ee1feba6cd3e84bc
Sergey <sdiomant@gmail.com>
2022-01-31 13:40:00 +0200

    bugFix

--- Homework32/Досконале число/js/script.js
+++ Homework32/Досконале число/js/script.js
@@ -1,19 +1,36 @@
-document.getElementById("myBtn").addEventListener("click", PerfectNumber);  
-
-function PerfectNumber(){
-  let num = document.getElementById('num').value;
-  num = parseInt(num);
+function setPerfectNumber (num) {
   let sum = 0;
-  let reminder;
-  for (let i = 0; i < num ; i++){
-    reminder = num%i;
-    if(reminder === 0){
-      sum +=i;
-    }
+
+  for (let i = 1; i <= num - 1; i++) {
+      if (num % i === 0) {
+          sum += i;
+      }
   }
-  if(num === sum){
-    alert(num + ' - Досконале число')
-  }else {
-    alert(num + ' - це не досконале число')
+  if (num === sum) {
+      return true;
+  } else {
+      return false;
   }
 }
+
+console.log(setPerfectNumber(6));
+
+
+
+function isNumPerfect (x, y) {
+
+  for (let i = x; i < y; i++) {
+      let sum = 0;
+
+      for (let y = 1; y <= i / 2; y++) {
+          if (i % y === 0) {
+              sum += y; 
+          }
+      }
+      if (i === sum) {
+          console.log(i)
+      }
+  }
+}
+
+isNumPerfect(10, 1000);
