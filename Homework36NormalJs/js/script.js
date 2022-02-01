60bc5aac31341e137f4c25e20f194c080a7e9e6d
Sergey <sdiomant@gmail.com>
2022-02-02 00:15:28 +0200

    bugfix

--- Homework36NormalJs/js/script.js
+++ Homework36NormalJs/js/script.js
@@ -1,42 +1,67 @@
-const academyAud = [
-  { name: 'MathAud', seats: 10, faculty:'Gryffindor', free: 'Yes',  listGroup: ['groupOne', 'groupTwo', 'groupThree']}, 
-  { name: 'SingAud', seats: 11, faculty:'Slytherin', free: 'No',  listGroup: ['groupOne', 'groupThree']}, 
-  { name: 'AlfAud', seats: 12, faculty:'Kogtevran', free: 'Yes',  listGroup: ['groupTwo', 'groupThree']}, 
-  { name: 'MaghAud', seats: 13, faculty:'Puffendui', free: 'No',  listGroup: ['groupThree']}, 
+
+
+
+
+
+const Hogwarts = [
+  {
+    name: "1",
+    sits: 7,
+    faculty: "Gryffindor",
+    isFree: true,
+    groops: [
+        "Magic-01", 
+        "Magic-02"
+      ],
+  },
+  {
+    name: "2",
+    sits: 10,
+    faculty: "Hufflepuff",
+    isFree: true,
+    groops: [
+        "Magic-01", 
+        "Magic-02",
+        "Magic-03"
+      ],
+  },
+  {
+    name: "3",
+    sits: 100,
+    faculty: "Ravenclaw ",
+    isFree: true,
+    groops: [
+      "Magic-01",
+      "Magic-02",
+      "Magic-03",
+      "Magic-04",
+    ],
+  },
+  {
+    name: "4",
+    sits: 45,
+    faculty: "Slytherin",
+    isFree: true,
+    groops: [
+      "Magic-01", 
+      "Magic-02", 
+      "Magic-03"
+      ],
+  },
 ];
 
 
-const out = document.getElementById('out');
-document.querySelector('button').onclick = () =>{
-   const showAcademy = [];
-   academyAud.forEach((auditory) => {
-      const {name, seats, faculty, free, listGroup} = auditory;
-      showAcademy.push(`
-      Name : ${name} 
-      Seats : ${seats} 
-      Faculty:${faculty} 
-      Free: ${free} 
-      ListGroup:${listGroup }  
-      `)
-   })
-   out.innerText = (showAcademy.join(""));
-}
+const audList = (arr) => {
+  arr.forEach((item) =>
+    console.log(`Аудиторія: ${item.name}, Місць: ${item.sits}, Факультет: ${item.faculty}, Чи вільна: ${item.isFree}, Групи, які можуть займатися: ${item.groops}`)
+  );
+};
 
+audList(Hogwarts);
 
+const audFaculty = (arr) => {
+  const result = arr.filter((Hogwarts) => Hogwarts.faculty === "Slytherin");
+  return result;
+};
+console.log(audFaculty(Hogwarts));
 
-const outSelect = document.getElementById('outSelect');
-
-document.getElementById('btnShowSelect').onclick = () =>{
-  const select = document.getElementById('select').value;
-  const faculty = academyAud.find(faculty => faculty.faculty === select)
-  const showFaculty = [];
-  const abjArr = Object.entries(faculty);
-  abjArr.forEach(([key,value]) => {
-    showFaculty.push(`
-     ${key} : ${value} 
-    `)
-  });
- 
-  outSelect.innerText = (showFaculty.join(""));
-  console.log(showFaculty);
-}
