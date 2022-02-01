742b1ab5ddf79904846d945e1429c131cadd8fe0
Sergey <sdiomant@gmail.com>
2022-02-02 00:02:31 +0200

    bug-fix

--- Homework35MaxJS/js/script.js
+++ Homework35MaxJS/js/script.js
@@ -1,7 +1,61 @@
-const btnBasket = document.querySelector('.basket');
-const basket = document.querySelector('.col-md-4');
-btnBasket.addEventListener('click', showBasket);
-
-function showBasket () {
-    basket.classList.toggle("none");
-}
+const Hogwarts = [
+    {
+      name: "1",
+      sits: 7,
+      faculty: "Gryffindor",
+      isFree: true,
+      groops: [
+          "Magic-01", 
+          "Magic-02"
+        ],
+    },
+    {
+      name: "2",
+      sits: 10,
+      faculty: "Hufflepuff",
+      isFree: true,
+      groops: [
+          "Magic-01", 
+          "Magic-02",
+          "Magic-03"
+        ],
+    },
+    {
+      name: "3",
+      sits: 100,
+      faculty: "Ravenclaw ",
+      isFree: true,
+      groops: [
+        "Magic-01",
+        "Magic-02",
+        "Magic-03",
+        "Magic-04",
+      ],
+    },
+    {
+      name: "4",
+      sits: 45,
+      faculty: "Slytherin",
+      isFree: true,
+      groops: [
+        "Magic-01", 
+        "Magic-02", 
+        "Magic-03"
+        ],
+    },
+  ];
+  
+  
+  const audList = (arr) => {
+    arr.forEach((item) =>
+      console.log(`Аудиторія: ${item.name}, Місць: ${item.sits}, Факультет: ${item.faculty}, Чи вільна: ${item.isFree}, Групи, які можуть займатися: ${item.groops}`)
+    );
+  };
+  
+  audList(Hogwarts);
+  
+  const audFaculty = (arr) => {
+    const result = arr.filter((Hogwarts) => Hogwarts.faculty === "Slytherin");
+    return result;
+  };
+  console.log(audFaculty(Hogwarts));
