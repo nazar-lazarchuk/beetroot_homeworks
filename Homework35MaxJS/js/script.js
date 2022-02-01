3d89ad14b02a3f0fd164d95538341dacd66588a5
Sergey <sdiomant@gmail.com>
2022-02-02 00:13:39 +0200

    bugfix2.0

--- Homework35MaxJS/js/script.js
+++ Homework35MaxJS/js/script.js
@@ -1,61 +1,63 @@
-const Hogwarts = [
-    {
-      name: "1",
-      sits: 7,
-      faculty: "Gryffindor",
-      isFree: true,
-      groops: [
-          "Magic-01", 
-          "Magic-02"
-        ],
-    },
-    {
-      name: "2",
-      sits: 10,
-      faculty: "Hufflepuff",
-      isFree: true,
-      groops: [
-          "Magic-01", 
-          "Magic-02",
-          "Magic-03"
-        ],
-    },
-    {
-      name: "3",
-      sits: 100,
-      faculty: "Ravenclaw ",
-      isFree: true,
-      groops: [
-        "Magic-01",
-        "Magic-02",
-        "Magic-03",
-        "Magic-04",
-      ],
-    },
-    {
-      name: "4",
-      sits: 45,
-      faculty: "Slytherin",
-      isFree: true,
-      groops: [
-        "Magic-01", 
-        "Magic-02", 
-        "Magic-03"
-        ],
-    },
-  ];
-  
-  
-  const audList = (arr) => {
-    arr.forEach((item) =>
-      console.log(`Аудиторія: ${item.name}, Місць: ${item.sits}, Факультет: ${item.faculty}, Чи вільна: ${item.isFree}, Групи, які можуть займатися: ${item.groops}`)
-    );
-  };
-  
-  audList(Hogwarts);
-  
-  const audFaculty = (arr) => {
-    const result = arr.filter((Hogwarts) => Hogwarts.faculty === "Slytherin");
-    return result;
-  };
-  console.log(audFaculty(Hogwarts));
+const shopList = [
+  { name: "bread", quantity: 1, isBought: true, price: 20, sum: 0 },
+
+  { name: "tomato", quantity: 1, isBought: true, price: 50, sum: 0 },
+
+  { name: "milk", quantity: 2, isBought: false, price: 19, sum: 0 },
+
+  { name: "banana", quantity: 10, isBought: false, price: 1, sum: 0 },
+
+  { name: "apple", quantity: 8, isBought: false, price: 3, sum: 0 },
+];
+
+
+const sortListByBought = (a, b) => (a.isBought > b.isBought ? 1 : -1);
+
+const sortedList = [...shopList].sort(sortListByBought);
+
+console.log(sortedList);
+
+
+const purchaseProducts = function (someName) {
+  for (i = 0; i < shopList.length; i++) {
+    if (shopList[i].name === someName) {
+      shopList[i].isBought = true;
+      return shopList[i]
+    }
+  }
+};
+
+const productBought = purchaseProducts("milk");
+console.log(shopList);
+
+if (productBought) {
+    console.log(`Був куплений продукт ${productBought.name} за ${productBought.price} грн`);
+} else {
+    console.log('Продукту з такою назвою не існує');
+}
+
+
+const sortNotPurcaseList = shopList.filter(
+  (list) => !list.isBought
+);
+console.log(sortNotPurcaseList);
+
+
+const deliteListItem = function (itemName) {
+  for (let j = 0; j < shopList.length; j++) {
+    if (shopList[j].name === itemName) {
+      return [...shopList].splice(j, 1);
+    }
+  }
+};
+
+
+console.log(deliteListItem('milk'));
+
+
+shopList.push(
+  { name: "banana", quantity: 2, isBought: true, price: 69, sum: 0 },
+  { name: "apple", quantity: 8, isBought: true, price: 83, sum: 0 }
+);
+
+console.log(shopList);
