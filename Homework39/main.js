aef252a130d0a49d88ff98dd734bfd1aee339c58
AnnaSavchenko <anna.pereklad@gmail.com>
2022-02-08 17:31:22 +0200

    Частина 1

---
+++ Homework39/main.js
@@ -0,0 +1,22 @@
+const popup = document.getElementById('modalWindow');
+const btnOpen = document.querySelector('.btn-open');
+const btnClose = popup.querySelector('.btn-close');
+
+
+ btnOpen.addEventListener('click', (e) => {
+     popup.style.display = "block";
+     btnOpen.style.display = "none";
+
+ });
+
+ btnClose.addEventListener('click', (e) => {
+     popup.style.display = "none";
+     btnOpen.style.display = "block";
+     
+ });
+
+ popup.addEventListener('click', (e) => {
+    popup.style.display = "none";
+    btnOpen.style.display = "block";
+    
+});
