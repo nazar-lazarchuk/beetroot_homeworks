9abfdb5b3c49fcb2780e52d9cae8768bf2ed9665
Sergey <sdiomant@gmail.com>
2022-02-17 20:30:43 +0200

    bugFix

--- Homework41/js/script.js
+++ Homework41/js/script.js
@@ -1,52 +1,54 @@
 ///form///
 
-document.addEventListener('DOMContentLoaded', function () {
-	const form = document.getElementById('form');
-	form.addEventListener('submit', formSend);
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", formSend);
 
-	async function formSend(e) {
-		let error = formValidate(form);
-		if (error === 0) {
-		} else {
-			alert('Заповніть обовязкові поля');
+  async function formSend(e) {
+    let error = formValidate(form);
+    if (error === 0) {
+    } else {
+      alert("Заповніть обовязкові поля");
       e.preventDefault();
-		}
+    }
+  }
 
-	}
+  function formValidate() {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
+    const inputName = document.querySelector("#formName");
 
-	function formValidate(form) {
-		let error = 0;
-		let formReq = document.querySelectorAll('._req');
+    for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
 
-		for (let index = 0; index < formReq.length; index++) {
-			const input = formReq[index];
-			formRemoveError(input);
-
-			if (input.classList.contains('_email')) {
-				if (emailTest(input)) {
-					formAddError(input);
-					error++;
-				}
-			} else {
-				if (input.value === '') {
-					formAddError(input);
-					error++;
-				}
-			}
-		}
-		return error;
-	}
-	function formAddError(input) {
-		input.parentElement.classList.add('_error');
-		input.classList.add('_error');
-	}
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('_error');
-		input.classList.remove('_error');
-	}
-	function emailTest(input) {
-		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-	}
-
+      if (input.classList.contains("_email")) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else {
+        if (input.value === "") {
+          formAddError(input);
+          error++;
+        }
+      }
+      if (inputName.value === "1") {
+        return alert("This field is not allowed to enter this character");
+      }
+    }
+    return error;
+  }
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
 });
 ////////////////////
