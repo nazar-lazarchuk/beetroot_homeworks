67590ebc336e2c4893be6ca15ac2c120a7918b16
Yuriy <yurchyk2906@gmail.com>
2022-02-23 16:27:52 +0200

    final

--- dz_41 – копія/main.js
+++ dz_41 – копія/main.js
@@ -3,25 +3,30 @@ const userEmail = document.querySelector("#email");
 const userComment = document.querySelector("#comment");
 const feedbackForm = document.querySelector(".form");
 const checkboxForm = document.querySelectorAll("#checkbox");
-const radioForm = document.querySelectorAll("#radio")
-const checkboxBlock = document.querySelector("#form1")
-const radioBlock = document.querySelector("#form2")
+const radioForm = document.querySelectorAll("#radio");
+const checkboxBlock = document.querySelector("#form1");
+const radioBlock = document.querySelector("#form2");
+const commentLimit = userComment.getAttribute("maxlength");
+const commentCounter = document.querySelector("#counter");
+
 
 const emailValidationBox = document.createElement("div");
 userEmail.after(emailValidationBox);
-
 const nameValidationBox = document.createElement("div");
 userName.after(nameValidationBox);
-
 const commentValidationBox = document.createElement("div");
 userComment.after(commentValidationBox);
+const radioValidationBox = document.createElement("div");
+radioBlock.after(radioValidationBox);
+const checkboxValidationBox = document.createElement("div");
+checkboxBlock.after(checkboxValidationBox);
 
-const checkboxValidation = document.createElement("div");
-checkboxBlock.after(checkboxValidation);
 
-const radioValidation = document.createElement("div");
-radioBlock.after(radioValidation); 
-
+commentCounter.innerText = commentLimit;
+userComment.addEventListener("input", () => {
+  const counterResult = commentLimit - userComment.value.length;
+  commentCounter.innerText = counterResult;
+});
 
 
 const getEmailValidationMessage = (email) => {
@@ -30,7 +35,6 @@ const getEmailValidationMessage = (email) => {
     return "Please write correct email address";
   }
 };
-
 const getNameValidationMessage = (name) => {
   if (
     /^[A-ZА-ЯІЇ\'\][a-zа-яії]{1,}\s[A-ZА-ЯІЇ\'\][a-zа-яії]{1,}$/g.test(
@@ -41,26 +45,33 @@ const getNameValidationMessage = (name) => {
     return "Please write correct name";
   }
 };
-
 const getTextAreaValidationMessage = (comment) => {
-  if(/[1]/g.test(userComment.value)){return "Please write comment without '1' "}
-  if (!/[A-Z][a-z]*/g.test(userComment.value)) { return "Please write correct comment"}
-  else { } 
+  if (/[1]/g.test(userComment.value)) {
+    return "Please write comment without '1' ";
+  }
+  if (!/[A-Z][a-z]*/g.test(userComment.value)) {
+    //відгук має починатись з великої букви і закінчуватись маленькою
+    return "Please write correct comment";
+  } else {
+  }
 };
-
 const getRadioValidationMessage = (radio) => {
   for (let i = 0; i < radioForm.length; i++) {
     if (radioForm[i].checked) {
+      return;
     }
   }
-  return "pick up radio";
+  return "Сhoose option";
+};
+const getСheckboxValidationMessage = (checkbox) => {
+  for (let i = 0; i < checkboxForm.length; i++) {
+    if (checkboxForm[i].checked) {
+      return;
+    }
+  }
+  return "Сhoose more than onecheckbox";
 };
 
-console.log(checkboxForm)
-const getСheckboxValidationMessage = (checkbox) => {
-  if (!checkboxForm.checked) { return "pick up checkbox";
-  } 
-};
 
 
 const setInvalid = (field, messageBox, message) => {
@@ -69,13 +80,15 @@ const setInvalid = (field, messageBox, message) => {
   messageBox.innerText = message;
 };
 
+
+
+
 feedbackForm.addEventListener("submit", (e) => {
   const email = userEmail.value;
   const name = userName.value;
   const comment = userComment.value;
-  const checkbox = checkboxForm;
   const radio = radioForm;
-
+  const checkbox = checkboxForm;
 
   const emailMessage = getEmailValidationMessage(email);
   const nameMessage = getNameValidationMessage(name);
@@ -95,12 +108,18 @@ feedbackForm.addEventListener("submit", (e) => {
     setInvalid(userComment, commentValidationBox, textAreaMessage);
     e.preventDefault();
   }
+
   if (radioMessage) {
-    setInvalid(radioForm, radioValidation, radioMessage);
-    e.preventDefault();
+    for (const option of radioForm) {
+      setInvalid(option, radioValidationBox, radioMessage);
+      e.preventDefault();
+    }
   }
+
   if (checkboxMessage) {
-    setInvalid(checkboxForm, checkboxValidation, checkboxMessage);
-    e.preventDefault();
+    for (const option of checkboxForm) {
+      setInvalid(option, checkboxValidationBox, checkboxMessage);
+      e.preventDefault();
+    }
   }
 });
